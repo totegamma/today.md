4cb4cfd90180a80af7522f728e3692edba0189c6
thotgamma <github@gammalab.net>
2022-06-19 17:42:50 +0900

    add today command

--- main.cpp
+++ main.cpp
@@ -7,7 +7,6 @@
 
 struct config_t {
 	std::string editor;
-	std::string workingDir;
 	std::string path;
 	std::vector<std::string> projects;
 	int projectID;
@@ -22,8 +21,8 @@ void registerOptions(CLI::App& app, config_t& conf) {
 	app.set_config("--config", ".today.conf", "Read an ini file")
 		-> transform(CLI::FileOnDefaultPath(getenv("HOME"), false));
 
-	app.add_option("--editor", conf.editor, "editor");
-	app.add_option("--workingDir", conf.workingDir, "project folder");
+	app.add_option("--editor", conf.editor, "editor")
+		-> default_val("vim");
 	app.add_option("--id", conf.projectID, "project ID")
 		-> default_val("0");
 	conf.projects_option = app.add_option("--projects", conf.projects, "project list");
@@ -38,26 +37,12 @@ void registerOptions(CLI::App& app, config_t& conf) {
 
 }
 
-int main(int argc, char** argv) {
-
-	CLI::App app{"today.md: document based todo management tool"};
-	config_t conf;
-
-	registerOptions(app, conf);
-
-	try {
-		app.parse(argc, argv);
-	} catch (const CLI::ParseError &e) {
-		return app.exit(e);
-	}
-
-	if (app.got_subcommand(conf.sub_init)) {
-		std::cout << "subcommand: init" << std::endl;
-		std::cout << "path: " << conf.path << std::endl;
-
+namespace commands {
+	int init(CLI::App& app, config_t& conf) {
 		std::string path = std::filesystem::absolute(conf.path);
 		if (std::find(conf.projects.begin(), conf.projects.end(), path) == conf.projects.end()) {
 
+			conf.conf_option->add_result("true");			
 			conf.projects_option->add_result(path);
 
 			std::ofstream out(std::string(getenv("HOME")) + "/.today.conf");
@@ -73,6 +58,34 @@ int main(int argc, char** argv) {
 		return 0;
 	}
 
+	int today(CLI::App& app, config_t& conf) {
+
+		std::string workingDir = conf.projects[conf.projectID];
+		std::cout << workingDir << std::endl;
+		
+		std::cout << "default" << std::endl;
+		std::cout << "editor: " << conf.editor << std::endl;
+		system(std::string(conf.editor + " " + workingDir + "/today.md").c_str());
+		return 0;
+	}
+}
+
+int main(int argc, char** argv) {
+
+	CLI::App app{"today.md: document based todo management tool"};
+	config_t conf;
+
+	registerOptions(app, conf);
+
+	try {
+		app.parse(argc, argv);
+	} catch (const CLI::ParseError &e) {
+		return app.exit(e);
+	}
+
+	if (app.got_subcommand(conf.sub_init))
+		return commands::init(app, conf);
+
 	if (!conf.configured) {
 		std::cout << "There are no configuration file!" << std::endl;
 		std::cout << "You can create it by hand, or just do 'today.md init' to Setup automatically." << std::endl;
@@ -80,12 +93,8 @@ int main(int argc, char** argv) {
 	}
 
 	// default
-	std::cout << "default" << std::endl;
-	std::cout << "editor: " << conf.editor << std::endl;
-	//system("vim test.txt");
+	return commands::today(app, conf);
 
-	std::cout << app.config_to_str(true, true) << std::endl;
-
-
+	// never reach
 	return 0;
 }
